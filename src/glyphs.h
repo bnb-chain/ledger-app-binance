565745e8d2cc98f3f3e55a96ae6a625293d685df
Luke Plaster <me@lukep.org>
2019-05-24 17:58:29 +0800

    NanoX support by LedgerHQ (#17)
    * Update variant
    
    * Add Nano X support
    
    * fix app name
    
    * Revert "Merge pull request #16 from binance-chain/nanox"
    
    This reverts commit 9dc886753fc2e799bb3e38d90e5ec4b2d1b0ace9, reversing
    changes made to dc1f6b18625209ae9c9f3288543705dc56785acb.
    
    * Remove duplicated env
    
    * tx bugfix on Nano S
    
    * small fix for Nano X + remove duplicate in Makefile

--- src/glyphs.h
+++ src/glyphs.h
@@ -76,7 +76,6 @@ extern unsigned char const C_icon_crossmark_bitmap[];
         #endif // GLYPH_icon_crossmark_BPP
         #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_dashboard_BPP
-<<<<<<< HEAD
         #define GLYPH_icon_dashboard_WIDTH 14
         #define GLYPH_icon_dashboard_HEIGHT 14
         #define GLYPH_icon_dashboard_BPP 1
@@ -120,42 +119,21 @@ extern unsigned char const C_icon_eye_bitmap[];
         extern const bagl_icon_details_t C_icon_eye;
         #endif // GLYPH_icon_eye_BPP
         #endif // OS_IO_SEPROXYHAL
-=======
-#define GLYPH_icon_dashboard_WIDTH 14
-#define GLYPH_icon_dashboard_HEIGHT 14
-#define GLYPH_icon_dashboard_BPP 1
-extern
-unsigned int const C_icon_dashboard_colors[]
-;
-extern	
-unsigned char const C_icon_dashboard_bitmap[];
-#ifdef OS_IO_SEPROXYHAL
-#include "os_io_seproxyhal.h"
-extern
-const bagl_icon_details_t C_icon_dashboard;
-#endif // GLYPH_icon_dashboard_BPP
-#endif // OS_IO_SEPROXYHAL
-
-#ifdef TARGET_NANOX
-
->>>>>>> Add compile define switch
 #ifndef GLYPH_icon_left_BPP
-       	#define GLYPH_icon_left_WIDTH 4
-       	#define GLYPH_icon_left_HEIGHT 7
-       	#define GLYPH_icon_left_BPP 1
-	extern unsigned int const C_icon_left_colors[];
-	extern unsigned char const C_icon_left_bitmap[];
-	#ifdef OS_IO_SEPROXYHAL
-	        #include "os_io_seproxyhal.h"
-	        extern const bagl_icon_details_t C_icon_left;
-      	#endif // OS_IO_SEPROXYHAL
-#endif // GLYPH_icon_left_BPP
-
+        #define GLYPH_icon_left_WIDTH 4
+        #define GLYPH_icon_left_HEIGHT 7
+        #define GLYPH_icon_left_BPP 1
+extern unsigned int const C_icon_left_colors[];
+extern unsigned char const C_icon_left_bitmap[];
+#ifdef OS_IO_SEPROXYHAL
+        #include "os_io_seproxyhal.h"
+        extern const bagl_icon_details_t C_icon_left;
+        #endif // GLYPH_icon_left_BPP
+        #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_right_BPP
-       	#define GLYPH_icon_right_WIDTH 4
+        #define GLYPH_icon_right_WIDTH 4
         #define GLYPH_icon_right_HEIGHT 7
         #define GLYPH_icon_right_BPP 1
-<<<<<<< HEAD
 extern unsigned int const C_icon_right_colors[];
 extern unsigned char const C_icon_right_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
@@ -163,21 +141,10 @@ extern unsigned char const C_icon_right_bitmap[];
         extern const bagl_icon_details_t C_icon_right;
         #endif // GLYPH_icon_right_BPP
         #endif // OS_IO_SEPROXYHAL
-=======
-	extern unsigned int const C_icon_right_colors[];
-	extern unsigned char const C_icon_right_bitmap[];
-	#ifdef OS_IO_SEPROXYHAL
-	        #include "os_io_seproxyhal.h"
-	        extern const bagl_icon_details_t C_icon_right;
-       	#endif // OS_IO_SEPROXYHAL
-#endif // GLYPH_icon_right_BPP
-
->>>>>>> Add compile define switch
 #ifndef GLYPH_icon_up_BPP
-       	#define GLYPH_icon_up_WIDTH 7
+        #define GLYPH_icon_up_WIDTH 7
         #define GLYPH_icon_up_HEIGHT 4
         #define GLYPH_icon_up_BPP 1
-<<<<<<< HEAD
 extern unsigned int const C_icon_up_colors[];
 extern unsigned char const C_icon_up_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
@@ -207,26 +174,3 @@ extern unsigned char const C_icon_warning_bitmap[];
         extern const bagl_icon_details_t C_icon_warning;
         #endif // GLYPH_icon_warning_BPP
         #endif // OS_IO_SEPROXYHAL
-=======
-	extern unsigned int const C_icon_up_colors[];
-	extern unsigned char const C_icon_up_bitmap[];
-	#ifdef OS_IO_SEPROXYHAL
-	        #include "os_io_seproxyhal.h"
-	        extern const bagl_icon_details_t C_icon_up;
-       	#endif // OS_IO_SEPROXYHAL
-#endif // GLYPH_icon_up_BPP
-
-#ifndef GLYPH_icon_down_BPP
-       	#define GLYPH_icon_down_WIDTH 7
-        #define GLYPH_icon_down_HEIGHT 4
-        #define GLYPH_icon_down_BPP 1
-	extern unsigned int const C_icon_down_colors[];
-	extern unsigned char const C_icon_down_bitmap[];
-	#ifdef OS_IO_SEPROXYHAL
-	        #include "os_io_seproxyhal.h"
-	        extern const bagl_icon_details_t C_icon_down;
-       	#endif // OS_IO_SEPROXYHAL
-#endif // GLYPH_icon_down_BPP
-
-#endif
->>>>>>> Add compile define switch
